242a17b21e8ff05c2a301abc147cf80e544c8f7c
charlmaths <122214610+charlmaths@users.noreply.github.com>
2023-07-22 08:29:39 +0100

    Created a new fibonacci seq. function

--- ProjectE_002.py
+++ ProjectE_002.py
@@ -27,7 +27,7 @@ def fib_seq2(x, y, n):
     """ This function will use while loop so we can specify how many terms we can do """
     num1 = x # 1st term
     num2 = y # 2nd term
-    i = 0 # Initialising while loop 
+    i = 0 # Initialising while loop.
     lst = [num1, num2] # 1st & 2nd term added into the list
     while i < n:
         num3 = num1 + num2 # Third term is the sum of first two terms
@@ -35,9 +35,20 @@ def fib_seq2(x, y, n):
         num1 = num2 # Then we shift to the next terms e.g. 1st term is now 2nd term 
         num2 = num3 # 2nd term is now 3rd term 
         i = i + 1 # This 'shifting' will continue until we reach 'n' 
-    return lst # return the lst
+    return lst
+
+def fib_even(z):
+    for num in z:
+        lst_new = []
+        if num % 2 == 0:
+            lst_new.append(num)
+    return lst_new
+            
+
 
 res2 = fib_seq2(0,1,5)
-sumvalue = sum(res2)
-print(sumvalue)
+print(res2)
+res3 = fib_even(res2)
+print(res3)
+
 
