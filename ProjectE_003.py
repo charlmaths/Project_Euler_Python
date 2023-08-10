c42b3d0188f8f4d6e6f13e82dbcbdd28cc0fdbde
charlmaths <122214610+charlmaths@users.noreply.github.com>
2023-08-10 12:18:05 +0100

    Created Class to solve Prime number problems

--- ProjectE_003.py
+++ ProjectE_003.py
@@ -2,18 +2,44 @@
 
 """ Prime number is only divisible by 1 or it self, it also must be greater than 1
 so 2, is only divisible by 1 x 2, 3 is only divisible by 1 x 3, 4 is divisible by 1 x 4 and 2 x 2
-so 4 is not a prime number. """
-
-num = 8
-
-""" We will start at 2 because every number is divisible by 1,
+so 4 is not a prime number. We will start at 2 because every number is divisible by 1,
 so we use 2 which is also the smallest prime number. We will also use
 'Else', a keyword for 'for'. 'Else' only triggers when the loop finishes,
-if break is present, it doesn trigger at all. """
+if break is present, it doesn trigger at all.
+"""
 
-for i in range(2, number):
-    if num % i == 0:
-        print("not prime")
-        break
-else:
-    print("prime")
+class PrimeNumberSolver():
+    """ This is a class that solves prime number related problems """
+
+    def __init__(self,number):
+        """ Instance variables """
+        self.number = number
+
+
+    def is_number_prime(self):
+        """ Method to check if number is a prime number """
+        num = self.number
+        for i in range(2, num):
+            if num % i == 0:
+                return "Not a prime Number"
+            else:
+                return "Number is prime"
+
+    def prime_factors(self):
+        """ Method to find the prime factors of a number
+         ******** NOT FINISHED YET **********
+         """
+        num = self.number
+        while num % 2 == 0:
+            print(2, end=" ")
+            num = num / 2
+
+
+
+# Object:
+
+# Instantiate the object
+
+#prime_number = PrimeNumberSolver(7)
+#check_value = prime_number.is_number_prime()
+#print(check_value)
